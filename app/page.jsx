85e2d36dcdfa725e4f5fd7f051a59c5059501a00
mie-intel <paryapradhanika@gmail.com>
2023-11-22 19:41:49 +0700

    adding corevalues section, product sections, and logo aset

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
+import Image from "next/image";
 import { Button } from "@material-tailwind/react";
+import Product from "./components/product";
+import CoreValues from "./components/coreValues";
 
 export default function Home() {
   return (
     <main>
-      <Button>Button</Button>
+      <CoreValues />
+      <Product />
     </main>
-  )
+  );
 }
