08452dc16fb9765b77fba4241e05df632f6c6549
mie-intel <paryapradhanika@gmail.com>
2023-11-25 00:51:09 +0700

    adding some asset

---
+++ app/components/Commitment.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+const CommitItem = ({ src, title, desc }) => {
+  return (
+    <>
+      <div className="w-[87vw] h-full flex flex-row justify-between">
+        <Image source={src} alt={title} width={600} height={400} />
+        <div className="w-[40vw] h-full flex flex-col text-primaryOne montserrat font-bold justify-center">
+          <div className="text-[30px] md:text-[40px]">{title}</div>
+          <div className="text-lg md:text-xl font-semibold">{desc}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Commitment = () => {
+  return (
+    <>
+      <div className="w-[99vw] flex flex-col justify-between">
+        <div className="w-[99vw] h-full bg-[#f1dec9] p-[45px] flex flex-col gap-[20px] justify-center items-center">
+          <div className="text-[45px] text-center text-[#8d7b68] montserrat font-bold">
+            Our Commitment
+          </div>
+          <div className="w-[87vw] h-full bg-white rounded-[10px]">
+            <CommitItem
+              src={"/asset_komitmen/petik_kopi.jpg"}
+              title={"Choosen Beans"}
+              desc="The coffee beans are chosen from the best coffee farm in Indonesia"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Commitment;
