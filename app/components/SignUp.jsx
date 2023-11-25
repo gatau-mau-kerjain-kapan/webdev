0e6c59f679be1969e3e3e6d2dcf66f1fdf2aa141
runs664 <harunsixsixfour@gmail.com>
2023-11-25 18:33:21 +0700

    Fix Google sign-in button onClick event

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -48,8 +48,8 @@ export default function SignUp() {
                 <div class="btn-wrapper text-center">
                     {/* <button class="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
                     <img alt="..." class="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/github.svg"/>Github </button> */}
-                    <button class="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
-                    <img alt="..." class="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/google.svg" onClick={()=> handleGoogleSignIn}/>Google</button>
+                    <button class="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"  onClick={handleGoogleSignIn} type="button">
+                    <img alt="..." class="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/google.svg"/>Google</button>
                     
                 </div>
                 <hr class="mt-6 border-b-1 border-blueGray-300"/>
