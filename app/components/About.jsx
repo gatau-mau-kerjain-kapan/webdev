f5371d9c33f2a9f43f2b9b511015395e6337180a
mie-intel <paryapradhanika@gmail.com>
2023-11-25 22:23:47 +0700

    adding ProdDetails Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function About() {
   return (
     <div id="about-section" className="flex justify-center my-20">
-      <div className="ms-10 md:ms-20 w-5/6 h-[600px] bg-[#c8b6a6] relative">
-        <div className=" bg-[#f1dec9] w-4/5 h-4/5 pl-5 md:pl-10 pt-10 md:pt-20 absolute top-3 md:top-20 -left-10 md:-left-24 flex-col ">
+      <div className="ms-10 md:ms-20 w-5/6 h-[600px] bg-[#c8b6a6] relative rounded-[10px]">
+        <div className=" bg-[#f1dec9] rounded-[10px] w-4/5 h-4/5 pl-5 md:pl-10 pt-10 md:pt-20 absolute top-3 md:top-20 -left-10 md:-left-24 flex-col ">
           <h1 className="text-[#8d7b68] text-3xl md:text-4xl mb-5 montserrat font-bold">
             What is Beanmaster anyway?
           </h1>
@@ -18,7 +18,7 @@ function About() {
         <img
           src="/img/coffee.jpg"
           alt="kopi"
-          className="md:w-[40vw] w-3/4 absolute -right-2 md:right-8 -bottom-2 md:top-3/4 md:-translate-y-1/2 z-10 pb-[25px]"
+          className="md:w-[40vw] w-3/4 absolute rounded-[10px] -right-2 md:right-8 -bottom-2 md:top-3/4 md:-translate-y-1/2 z-10 pb-[25px]"
         />
       </div>
       {/* <div className="hidden lg:inline ms-5">
