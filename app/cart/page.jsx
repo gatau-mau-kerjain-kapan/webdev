e96631f66e9c4d2a4f22fdd8f65ac6e0883e347e
mie-intel <paryapradhanika@gmail.com>
2024-02-22 14:20:06 +0700

    fix: cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -67,6 +67,23 @@ export default function Cart() {
         document.getElementById("cod").checked)
     ) {
       if (document.getElementById("wa").checked) {
+        window.open(
+          "https://wa.me/6285876048549?text=Hello%2C%20I%20want%20to%20verify%20my%20order%20for%20these%20product%3A%20" +
+            urlEncodeString(
+              Object.values(cart)
+                .map((item) => item.name + "%20x" + item.quantity)
+                .join(", ")
+            ) +
+            ".%20The%20total%20price%20of%20this%20order%20is%20" +
+            (total + shipping).toLocaleString("id", {
+              style: "currency",
+              currency: "IDR",
+            }) +
+            ".%20I%20also%20want%20this%20product%20to%20be%20sent%20to%20" +
+            urlEncodeString(document.getElementById("address").value) +
+            ".%20Thank%20you!"
+        );
+
         remove(ref(db, "cart/" + currentUser.uid));
       } else {
         Swal.fire({
@@ -88,7 +105,6 @@ export default function Cart() {
     }
   };
 
-<<<<<<< HEAD
   useEffect(() => {
     try {
       onValue(
@@ -99,21 +115,6 @@ export default function Cart() {
         },
         {
           onlyOnce: true,
-=======
-            onValue(ref(db, 'cart/' + currentUser.uid), (snapshot) => {
-                const data = snapshot.val();
-                setCart(data);
-<<<<<<< HEAD
-                setTotal(data.reduce((a, b) => a + (b.price * b.quantity), 0) ?? 0)
-=======
-                Object.values(data).forEach((item) => {
-                    setTotal(total + item.price * item.quantity);
-                }
->>>>>>> parent of 030bf69 (fixing typo)
-            })
-        } catch (err) {
-            console.log(err)
->>>>>>> parent of fa36fc0 (enhancement)
         }
       );
 
@@ -169,7 +170,6 @@ export default function Cart() {
           <h1 class="mb-10 text-center text-2xl font-bold">Cart Items</h1>
           <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
             <div class="rounded-lg md:w-2/3">
-<<<<<<< HEAD
               {Object.values(cart).map((item) => {
                 return (
                   <div key={item.key}>
@@ -187,37 +187,6 @@ export default function Cart() {
                             {item.name}
                           </h2>
                           <p class="mt-1 text-xs text-gray-700">{item.name}</p>
-=======
-                {Object.values(cart).map((item) => {
-                    return (
-                        <div key={item.key}>
-                        <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-                        <Image
-                            src={item.source}
-                            alt={item.name}
-                            width={300}
-                            height={300}
-                            className="w-full rounded-lg sm:w-40"
-                        />
-                        <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                            <div class="mt-5 sm:mt-0">
-                            <h2 class="text-lg font-bold text-gray-900">{item.name}</h2>
-                            <p class="mt-1 text-xs text-gray-700">{item.name}</p>
-                            </div>
-                            <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                            <div class="flex items-center border-gray-100">
-                                <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => subQuantity(item.key)}> - </span>
-                                <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={item.quantity} min="1" />
-                                <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => addQuantity(item.key)}> + </span>
-                            </div>
-                            <div class="flex items-center space-x-4">
-                                <p class="text-sm">{(item.price??0).toLocaleString('id', {style: 'currency', currency: 'IDR'})}</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" onClick={() => removeItem(item.key)}/>
-                                </svg>
-                            </div>
-                            </div>
->>>>>>> parent of fa36fc0 (enhancement)
                         </div>
                         <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                           <div class="flex items-center border-gray-100">
